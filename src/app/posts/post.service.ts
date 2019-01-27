775094e4893fb67efd123800f7536aab9fc9c6f2
andrii <anderssona258@gmail.com>
2019-01-27 14:11:15 +0200

    create delete method on server && fetch this on front

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Post } from './post.model';
 import { Subject, from } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,18 @@ export class PostService {
   ) { }
 
   getPosts() {
-    this.http.get<{message: string, data: Post[]}>('http://localhost:3000/api/posts')
-        .subscribe((res) => {
-          this.posts = res['data'];
+    this.http.get<{message: string, data: any}>('http://localhost:3000/api/posts')
+        .pipe(map(res => {
+          return res['data'].map(post => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id
+            }
+          });
+        }))
+        .subscribe((postData) => {
+          this.posts = postData;
           this.postsUpdated.next([...this.posts]);
         });
   }
@@ -32,10 +42,25 @@ export class PostService {
       title: title,
       content: content
     };
-    this.http.post<{message: string, data: Post}>('http://localhost:3000/api/posts', newPost)
-        .subscribe((res) => {
-          this.posts.push(res['data']);
+    this.http.post<{message: string, data: any}>('http://localhost:3000/api/posts', newPost)
+        .pipe(map(res => {
+          return {
+              title: res['data'].title,
+              content: res['data'].content,
+              id: res['data']._id
+          }; 
+        }))
+        .subscribe((post) => {
+          this.posts.push(post);
           this.postsUpdated.next([...this.posts]);
         });
+  }
+
+  deletePost(postId: string) {
+    return this.http.delete('http://localhost:3000/api/posts/' + postId)
+                .subscribe(res => {
+                  this.posts = this.posts.filter(el => el.id !== postId);
+                  this.postsUpdated.next([...this.posts]);
+                });
   }
 }
